b3d7629cee8ee1ee2487313c5c4922c359e93e63
Zephyrus\Miguel <misan484@hotmail.com>
2022-02-07 11:06:46 +0100

    Ejercicio5

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D playerRB;
+    SpriteRenderer playerRenderer;
     public float force = 10;
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        playerRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
         float movement = Input.GetAxis("Horizontal"); //capturamos teclado
         playerRB.velocity = (transform.right * movement * force); //aplicamos fuerza para mover el player
+        if(movement < 0)
+        {
+            playerRenderer.flipX = true;
+        }
+        else
+        {
+            playerRenderer.flipX = false;
+        }
         
     }
 
